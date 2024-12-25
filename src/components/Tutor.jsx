decd60ec69ac478b00f62443e9ea9564ed2d999d
Yead <yead19@gmail.com>
2024-12-25 19:35:59 +0600

    bug fixed

--- src/components/Tutor.jsx
+++ src/components/Tutor.jsx
@@ -3,14 +3,26 @@ import { Outlet, useLoaderData } from 'react-router-dom';
 import TutorCard from '../components/TuitorCard';
 import useAuth from '../hooks/UseAuth';
 import axios from 'axios';
+import Loading from './Loading';
 const Tutor = () => {
     const data = useLoaderData()
-    const { search, setSearch, tutorCounter } = useAuth()
+    const { search, setSearch, } = useAuth()
 
     const [tutorials, setTutorials] = useState(data)
     const [currentPage, setCurrentPage] = useState(0)
+    const [loading, setLoading] = useState(false);
+    const [tutorCounter, setTutorCount] = useState({})
 
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/tutors-count')
+            .then(res => setTutorCount(res.data))
+    }, [])
     const { tutorCount } = tutorCounter
+
+    console.log(tutorCounter);
+
+
     const itemsPerPage = 9;
 
     const handleNextBtn = () => {
@@ -27,12 +39,20 @@ const Tutor = () => {
     }
 
     const numberOfPages = Math.ceil(tutorCount / itemsPerPage)
+    // const numberOfPages = Math.ceil((tutorCount || 0) / itemsPerPage);
 
-    const pages = [...Array(numberOfPages).keys()]
-    // console.log(pages);
+    const pages = numberOfPages > 0 ? [...Array(numberOfPages).keys()] : [];
+    // const pages = [...Array(numberOfPages).keys()]
+
+
+    console.log(pages);
     useEffect(() => {
+        setLoading(true)
         axios.get(`http://localhost:5000/tutorials?page=${currentPage}&size=${itemsPerPage}`)
-            .then(res => setTutorials(res.data))
+            .then(res => {
+                setTutorials(res.data)
+                setLoading(false)
+            })
     }, [currentPage, itemsPerPage])
 
 
@@ -51,26 +71,36 @@ const Tutor = () => {
 
     return (
         <div>
-            {
-                tutorials?.length > 0 ?
-
-                    <div className="  grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full  gap-x-6 gap-y-14">
-                        {tutorials?.map((tutor) => (
-                            <TutorCard tutor={tutor} key={tutor._id}></TutorCard>
-                        ))}
-                    </div>
-                    :
-                    <p className='text-xl text-red-500'>No Data Found. Reset input and Click Search to load all data again.</p>
-            }
+            {loading ? (
+                <Loading></Loading>
+            ) : tutorials?.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full gap-x-6 gap-y-14">
+                    {tutorials?.map((tutor) => (
+                        <TutorCard tutor={tutor} key={tutor._id}></TutorCard>
+                    ))}
+                </div>
+            ) : (
+                <p className="text-xl text-red-500">
+                    No Data Found. Reset input and Click Search to load all data again.
+                </p>
+            )}
 
             <div className="join my-8 flex justify-center items-center">
                 <button onClick={handlePrevious} className="join-item btn btn-md">Prev</button>
-                {
-
+                {loading ? (
+                    <Loading></Loading>
+                ) : (
                     pages?.map((page, index) => (
-                        <button onClick={() => setCurrentPage(page)} key={index} className={`join-item btn btn-md ${currentPage === page ? 'btn-active' : undefined}`}>{page + 1}</button>
+                        <button
+                            onClick={() => setCurrentPage(page)}
+                            key={index}
+                            className={`join-item btn btn-md ${currentPage === page ? 'btn-active' : undefined
+                                }`}
+                        >
+                            {page + 1}
+                        </button>
                     ))
-                }
+                )}
 
                 <button onClick={handleNextBtn} className="join-item btn btn-md">Next</button>
 
